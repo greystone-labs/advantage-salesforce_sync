09142532ffbb5c43a17b8c78387f24c2caa7fdec
Tom McArdle <tom.mcardle@greyco.com>
2022-04-11 11:15:51 -0400

    moved authenticate method into shared context

---
+++ spec/context/authenticate.rb
@@ -0,0 +1,8 @@
+# frozen_string_literal: true
+
+RSpec.shared_context 'authentication' do
+  # Mock Restforce::Client.authenticate! for testing
+  def authenticate!
+    allow_any_instance_of(Restforce::Client).to receive(:authenticate!).and_return(true)
+  end
+end
